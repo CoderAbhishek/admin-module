6529c73c69a6d6dd65f67c94b4a55cce7b066580
Abhishek <abhishekvinod48@gmail.com>
2020-12-17 18:42:09 +0530

    layout

--- my-module/src/app/app-routing.module.ts
+++ my-module/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { AdminComponent } from './admin/admin.component';
+import { LogoutComponent } from './admin/logout/logout.component';
+import { SettingsComponent } from './admin/settings/settings.component';
+import { UserAdminComponent } from './admin/user-admin/user-admin.component';
+// import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: AppComponent},
+  { path: 'user-admin', component: UserAdminComponent},
+  { path: 'settings', component: SettingsComponent},
+  { path: 'logout', component: LogoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-module/src/app/app.module.ts
+++ my-module/src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { SettingsComponent } from './admin/settings/settings.component';
+import { UserAdminComponent } from './admin/user-admin/user-admin.component';
+import { LogoutComponent } from './admin/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    SettingsComponent,
+    UserAdminComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
